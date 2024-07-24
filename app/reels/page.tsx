06b69516cadcfe3530b78a68a20fee68ff63a71f
Suso <susinodevelop@gmail.com>
2024-07-24 16:49:23 +0000

    Corrige llamadas pestaña reels

--- app/reels/page.tsx
+++ app/reels/page.tsx
@@ -5,58 +5,56 @@ import { Box, Text } from '@chakra-ui/react';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Reels',
-  description: 'Página de reels',
+    title: 'Reels',
+    description: 'Página de reels',
 };
 
 const Reels = async () => {
-  // Mapa para cachear los usuarios
-  const users = new Map<number, User>();
+    // Mapa para cachear los usuarios
+    const users = new Map<number, User>();
 
-  const getUserFromMap = async (id: number): Promise<User> => {
-    if (users.has(id)) {
-      return users.get(id) as User;
-    } else {
-      const user = await getUser(id);
-      users.set(id, user);
-      return user;
+    const saveCacheUser = async (id: number) => {
+        if (!users.has(id)) {
+            const user = await getUser(id);
+            users.set(user.id, user);
+        }
     }
-  };
 
-  const getReels = async (): Promise<ReelUser[]> => {
-    const retrievedReels: Reel[] = await getAllReels();
+    const getReels = async (): Promise<ReelUser[]> => {
+        const retrievedReels: Reel[] = await getAllReels();
 
-    const reelsWithUsers = await Promise.all(
-      retrievedReels.map(async (retrievedReel) => {
-        const user = await getUserFromMap(retrievedReel.user_owner_id);
-        return { ...retrievedReel, user };
-      })
+        for (const reel of retrievedReels) {
+            saveCacheUser(reel.id)
+        }
+        const reelsWithUsers = retrievedReels.map((reel) => {
+            const user = users.get(reel.user_owner_id);
+            return { ...reel, user } as ReelUser;
+        });
+
+        return reelsWithUsers;
+    };
+
+    const reels = await getReels();
+
+    return (
+        <Box
+            position="absolute"
+            left="0"
+            top="0"
+            width="100%"
+            display="flex"
+            flexDirection="column"
+            gap="20px"
+            alignItems="center"
+        >
+            <Text as="h1">Reels</Text>
+            {reels.map((reel) => (
+                <div key={reel.id}>
+                    <Reel reel={reel} user={reel.user} />
+                </div>
+            ))}
+        </Box>
     );
-
-    return reelsWithUsers;
-  };
-
-  const reels = await getReels();
-
-  return (
-    <Box
-      position="absolute"
-      left="0"
-      top="0"
-      width="100%"
-      display="flex"
-      flexDirection="column"
-      gap="20px"
-      alignItems="center"
-    >
-      <Text as="h1">Reels</Text>
-      {reels.map((reel) => (
-        <div key={reel.id}>
-          <Reel reel={reel} user={reel.user} />
-        </div>
-      ))}
-    </Box>
-  );
 };
 
 export default Reels;
