16a9754c3d861a32b089d8fb6be2854dc8f04c86
Suso <susinodevelop@gmail.com>
2024-07-25 23:27:07 +0000

    Correccion layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,7 @@ export const metadata: Metadata = {
   title: "Instagram Clone",
   description: "Esta página es un proyecto personal de susinodevelop",
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <html lang="es">
       <body className={`${inter.className} bg-black text-white`}>
@@ -31,3 +26,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+export default RootLayout;
