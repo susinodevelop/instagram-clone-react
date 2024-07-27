88e9ac28b0e996fdd863eb653b26e80e1bf89cea
Suso <susinodevelop@gmail.com>
2024-07-27 16:38:11 +0000

    Correccion

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || `https://${process.env.VERCEL_URL}`,
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
