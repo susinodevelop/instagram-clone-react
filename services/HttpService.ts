4d1a5c94ba90d879a5fff5efe0d9e5634cadca06
Suso <susinodevelop@gmail.com>
2024-07-27 14:05:56 +0000

    correcciones http

--- services/HttpService.ts
+++ services/HttpService.ts
@@ -1,157 +1,98 @@
 const isServer = typeof window === 'undefined';
 const baseURL = isServer ? process.env.NEXT_PUBLIC_API_URL : '';
 
+if (isServer && !baseURL) {
+    throw new Error('NEXT_PUBLIC_API_URL is not defined');
+}
+
+const defaultHeaders = {
+    'Content-Type': 'application/json',
+};
+
+const fetchWithBaseUrl = async (url: string, options?: RequestInit) => {
+    const response = await fetch(`${baseURL}${url}`, options);
+    if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Failed to fetch: ${response.status} ${response.statusText} - ${errorText}`);
+    }
+    return response;
+};
+
 export const useGET = async <T>(url: string): Promise<T> => {
-    const response = await fetch(`${baseURL}${url}`);
-    if (!response.ok) {
-        throw new Error(`Failed to GET ${baseURL}${url}`);
-    }
+    const response = await fetchWithBaseUrl(url);
     try {
-        const result = await response.json();
-        return result as T;
+        return (await response.json()) as T;
     } catch (error) {
         console.error(error);
-        throw new Error(`Failed to parse JSON from ${baseURL}${url}`);
+        throw new Error(`Failed to parse JSON from ${url}`);
     }
-}
+};
 
-
-interface PostProps {
+interface RequestProps {
     url: string;
     pathParams?: Record<string, string>;
     queryParams?: Record<string, string>;
     bodyParams?: any;
 }
 
-export const usePOST = async <T>(props: PostProps): Promise<T> => {
-    const { pathParams = {}, queryParams = {}, bodyParams = {} } = props;
+const buildUrlWithParams = (url: string, pathParams?: Record<string, string>, queryParams?: Record<string, string>) => {
+    let urlTemplate = url;
 
-    // Plantilla de URL
-    let urlTemplate = `${baseURL}${props.url}`;
-
-    // Reemplazar los placeholders en pathParams
-    Object.keys(pathParams).forEach(key => {
-        const value = pathParams[key];
-        urlTemplate = urlTemplate.replace(`{${key}}`, value);
-    });
-
-    // Construir la cadena de consulta (query string)
-    const queryString = new URLSearchParams(queryParams).toString();
-    if (queryString) {
-        urlTemplate += `?${queryString}`;
+    if (pathParams) {
+        Object.keys(pathParams).forEach(key => {
+            urlTemplate = urlTemplate.replace(`{${key}}`, pathParams[key]);
+        });
     }
 
-    // Realizar la solicitud POST con fetch
-    const response = await fetch(urlTemplate, {
+    if (queryParams) {
+        const queryString = new URLSearchParams(queryParams).toString();
+        if (queryString) {
+            urlTemplate += `?${queryString}`;
+        }
+    }
+
+    return urlTemplate;
+};
+
+export const usePOST = async <T>(props: RequestProps): Promise<T> => {
+    const urlTemplate = buildUrlWithParams(props.url, props.pathParams, props.queryParams);
+    const response = await fetchWithBaseUrl(urlTemplate, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(bodyParams),
+        headers: defaultHeaders,
+        body: JSON.stringify(props.bodyParams),
     });
-
-    if (!response.ok) {
-        throw new Error(`Failed to fetch post at ${baseURL}${urlTemplate}`);
-    }
-
     try {
-        const result = await response.json();
-        return result as T;
+        return (await response.json()) as T;
     } catch (error) {
         console.error(error);
         throw new Error(`Failed to parse JSON from ${props.url}`);
     }
 };
 
-
-interface PutProps {
-    url: string;
-    pathParams?: Record<string, string>;
-    queryParams?: Record<string, string>;
-    bodyParams?: any;
-}
-
-export const usePUT = async <T>(props: PutProps): Promise<T> => {
-    const { pathParams = {}, queryParams = {}, bodyParams = {} } = props;
-
-    // Plantilla de URL
-    let urlTemplate = `${baseURL}${props.url}`;
-
-    // Reemplazar los placeholders en pathParams
-    Object.keys(pathParams).forEach(key => {
-        const value = pathParams[key];
-        urlTemplate = urlTemplate.replace(`{${key}}`, value);
-    });
-
-    // Construir la cadena de consulta (query string)
-    const queryString = new URLSearchParams(queryParams).toString();
-    if (queryString) {
-        urlTemplate += `?${queryString}`;
-    }
-
-    // Realizar la solicitud POST con fetch
-    const response = await fetch(urlTemplate, {
+export const usePUT = async <T>(props: RequestProps): Promise<T> => {
+    const urlTemplate = buildUrlWithParams(props.url, props.pathParams, props.queryParams);
+    const response = await fetchWithBaseUrl(urlTemplate, {
         method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(bodyParams),
+        headers: defaultHeaders,
+        body: JSON.stringify(props.bodyParams),
     });
-
-    if (!response.ok) {
-        throw new Error(`Failed to fetch put at ${baseURL}${urlTemplate}`);
-    }
-
     try {
-        const result = await response.json();
-        return result as T;
+        return (await response.json()) as T;
     } catch (error) {
         console.error(error);
         throw new Error(`Failed to parse JSON from ${props.url}`);
     }
 };
 
-interface PatchProps {
-    url: string;
-    pathParams?: Record<string, string>;
-    queryParams?: Record<string, string>;
-    bodyParams?: any;
-}
-
-export const usePATCH = async <T>(props: PatchProps): Promise<T> => {
-    const { pathParams = {}, queryParams = {}, bodyParams = {} } = props;
-
-    // Plantilla de URL
-    let urlTemplate = `${baseURL}${props.url}`;
-
-    // Reemplazar los placeholders en pathParams
-    Object.keys(pathParams).forEach(key => {
-        const value = pathParams[key];
-        urlTemplate = urlTemplate.replace(`{${key}}`, value);
-    });
-
-    // Construir la cadena de consulta (query string)
-    const queryString = new URLSearchParams(queryParams).toString();
-    if (queryString) {
-        urlTemplate += `?${queryString}`;
-    }
-
-    // Realizar la solicitud POST con fetch
-    const response = await fetch(urlTemplate, {
+export const usePATCH = async <T>(props: RequestProps): Promise<T> => {
+    const urlTemplate = buildUrlWithParams(props.url, props.pathParams, props.queryParams);
+    const response = await fetchWithBaseUrl(urlTemplate, {
         method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(bodyParams),
+        headers: defaultHeaders,
+        body: JSON.stringify(props.bodyParams),
     });
-
-    if (!response.ok) {
-        throw new Error(`Failed to fetch PATCH at ${baseURL}${urlTemplate}`);
-    }
-
     try {
-        const result = await response.json();
-        return result as T;
+        return (await response.json()) as T;
     } catch (error) {
         console.error(error);
         throw new Error(`Failed to parse JSON from ${props.url}`);
@@ -159,16 +100,11 @@ export const usePATCH = async <T>(props: PatchProps): Promise<T> => {
 };
 
 export const useDELETE = async <T>(url: string): Promise<T> => {
-    const response = await fetch(`${baseURL}${url}`);
-    if (!response.ok) {
-        throw new Error(`Failed to DELETE ${url}`);
-    }
-
+    const response = await fetchWithBaseUrl(url, { method: 'DELETE' });
     try {
-        const result = await response.json();
-        return result as T;
+        return (await response.json()) as T;
     } catch (error) {
         console.error(error);
-        throw new Error(`Failed to parse JSON from ${baseURL}${url}`);
+        throw new Error(`Failed to parse JSON from ${url}`);
     }
-}
+};
