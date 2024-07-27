bc2bddac2cd6862f364791d65be3e7dfc35450ae
Suso <susinodevelop@gmail.com>
2024-07-27 15:08:04 +0000

    añade logs

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,8 +4,10 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   try {
 
+    console.log("Obteniendo datos de usuarios")
     const result = await sql`SELECT * FROM users`
 
+    console.log("Datos de usuarios obtenidos" + JSON.stringify(result))
     const users = result.rowCount === 0 ? [] : result.rows
 
     return new NextResponse(JSON.stringify(users), {
@@ -15,6 +17,7 @@ export async function GET(request: Request) {
       },
     });
   } catch (error) {
+    console.log("ERROR ABURRIDO" + JSON.stringify(error))
     return new NextResponse(JSON.stringify({ error: 'Failed to fetch data' }), {
       status: 500,
       headers: {
