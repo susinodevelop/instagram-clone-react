459b2b757ca24d9b42799f8fe0cb8980eb7adcd8
Suso <susinodevelop@gmail.com>
2024-07-28 20:47:48 +0000

    Correccion

--- services/HttpService.ts
+++ services/HttpService.ts
@@ -2,7 +2,7 @@ if (!process.env.VERCEL_URL && !process.env.NEXT_PUBLIC_API_URL) {
     throw new Error('NEXT_PUBLIC_API_URL is not defined');
 }
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || `https://${process.env.VERCEL_URL}`
+const baseURL = process.env.NEXT_PUBLIC_API_URL || `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 
 const defaultHeaders = {
     'Content-Type': 'application/json',
