c48c0f90eaaf809ec9cf5df9bbf26f2b1cc0253f
Suso <susinodevelop@gmail.com>
2024-07-28 22:14:23 +0000

    prueba

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 import Header from '@/components/Header'
 import Feed from '@/components/Feed'
 import { Flex } from '@chakra-ui/react'
