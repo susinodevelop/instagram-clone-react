49476cf2a01ad95d5a6d93daa1ff84e7ca859bbb
Suso <susinodevelop@gmail.com>
2024-07-30 22:59:52 +0000

    Elimina componente innecesario

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,20 +1,30 @@
 import Header from '@/components/Header'
-import FeedView from '@/components/FeedView'
-import { Flex } from '@chakra-ui/react'
+import { Flex, VStack } from '@chakra-ui/react'
 import Suggestions from "@/components/Suggestions";
 import { Metadata } from 'next';
+import { getAllPosts } from '@/services/PostService';
+import PostView from '@/components/PostView';
 
 export const metadata: Metadata = {
     title: 'Inicio',
     description: 'Página del feed de posts',
 }
 
-const Feed = () => {
+const Feed = async () => {
+
+    const posts = await getAllPosts()
+
     return (
         <Flex>
             <Flex direction="column" flex="1">
                 <Header />
-                <FeedView />
+                <VStack spacing="10px" padding="20px" bg="black" color="white" flex="1">
+                    {
+                        posts.map(post => (
+                            <PostView key={post.id} post={post} />
+                        ))
+                    }
+                </VStack>
             </Flex>
             <Suggestions />
         </Flex>
